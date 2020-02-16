40f282499280645598a003fd8b8f4ed565d5b31e
Zelita Tyananda Dytasya Putri <ztdputri010403@gmail.com>
2020-02-15 21:06:46 -0800

    concat beres wkwk

--- concat_two_arr.js
+++ concat_two_arr.js
@@ -1,13 +1,13 @@
 // buat function untuk "concat" dua buah array menjadi satu array
 
-
-function concat(arr1, arr2) {
-	
+function arrConcat(arr1, arr2) {
+  var result = arr1.concat(arr2);
+  console.log(result);
 }
 
 // TIPS:
 // keyword google "Javascript Concat"
 
-concat([1, 3, 5], [2, 6, 8]) // [1, 3, 5, 2, 6, 8]
-concat([7, 8], [10, 9, 1, 1, 2]) // [7, 8, 10, 9, 1, 1, 2]
-concat([4, 5, 1], [3, 3, 3, 3, 3]) // [4, 5, 1, 3, 3, 3, 3, 3]
+arrConcat([1, 3, 5], [2, 6, 8]); // [1, 3, 5, 2, 6, 8]
+arrConcat([7, 8], [10, 9, 1, 1, 2]); // [7, 8, 10, 9, 1, 1, 2]
+arrConcat([4, 5, 1], [3, 3, 3, 3, 3]); // [4, 5, 1, 3, 3, 3, 3, 3]
