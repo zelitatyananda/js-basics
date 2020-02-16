b7ea3fb0afda5ded5456c33c7646aaff2852da36
Zelita Tyananda Dytasya Putri <ztdputri010403@gmail.com>
2020-02-15 23:32:20 -0800

    doneee

--- buggy_squared.js
+++ buggy_squared.js
@@ -1,10 +1,10 @@
 // 2. dibawah ini adalah function untuk mencari kuadrat dari sebuah bilangan.
 // kamu diminta untuk "fixing bugs" dari code dibawah ini.
-function squaed(b) {
-	return $a*$a;
+function squared(b) {
+  console.log(b * b);
+  return b * b;
 }
 
-
-squared(5) // 25
-squared(9) // 81
-squared(100) // 10000
+squared(5); // 25
+squared(9); // 81
+squared(100); // 10000
