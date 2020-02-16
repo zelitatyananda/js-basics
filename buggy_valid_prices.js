4774a1c19f8209ea8fa850421c5e06c8faa3bc7a
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-02-16 10:00:10 +0700

    Add bugs

--- buggy_valid_prices.js
+++ buggy_valid_prices.js
@@ -1,7 +1,7 @@
 // kamu diminta untuk "fixing bugs" dari code dibawah ini
 // sehingga function ini bisa mereturn boolean jika "price-nya" valid.
 
-function hasValidPrice(product) {
+unction hasValidPrice(product) {
 	return (product && product.price && product.price >= 0)
 }
 
@@ -10,4 +10,4 @@ hasValidPrice({ "product": "Milk", price: 1.50 }) // true
 hasValidPrice({ "product": "Cheese", price: -1 }) // false
 hasValidPrice({ "product": "Eggs", price: 0 }) // true
 hasValidPrice({ "product": "Cerials", price: '3.0' }) // false
-hasValidPrice() // false
+hasValidPrice() ➞ false
